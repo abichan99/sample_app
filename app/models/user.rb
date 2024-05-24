bbf640d4128ade7417f58172adccf6cee90c95a0
abichan99 <132280033+abichan99@users.noreply.github.com>
2024-05-24 11:37:51 +0000

    6章クリア

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: email_regex }
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobarrr", password_confirmation: "foobarrr")
   end
 
   test "should be valid" do
@@ -51,5 +51,15 @@ class UserTest < ActiveSupport::TestCase
   test "email address should be downcase" do
     @user.update(email: "user@E.COM")
     assert_equal @user.reload.email, "user@e.com"
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
