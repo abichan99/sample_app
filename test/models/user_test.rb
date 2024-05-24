c040466276b703f6284b50dba77366583a970de4
abichan99 <132280033+abichan99@users.noreply.github.com>
2024-05-24 11:01:00 +0000

    6.3.1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -35,10 +35,21 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email address should be valid" do
+    @user.save
+    dup_user = @user.dup
+    assert_not dup_user.valid?
+  end
+  
+  test "email address should be downcase" do
+    @user.update(email: "user@E.COM")
+    assert_equal @user.reload.email, "user@e.com"
   end
 end
