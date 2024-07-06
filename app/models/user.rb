6f6824c4c0c98c3aa3b8d623b01b58eaaf80b668
abichan99 <132280033+abichan99@users.noreply.github.com>
2024-07-06 11:24:47 +0000

    8章終わり

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: email_regex }
   validates :password, presence: true, allow_nil: true, length: { minimum: 5 }
   has_secure_password
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
