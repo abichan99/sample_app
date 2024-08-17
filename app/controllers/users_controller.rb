4756460eadf41b9594a5b1dc8c50f372161c8004
abichan99 <132280033+abichan99@users.noreply.github.com>
2024-08-17 10:33:12 +0000

    13.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page]) # view で使しか使わない
 
     unless @user.activated
       flash[:warning] = 'User is not activated'
