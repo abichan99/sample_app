2ae9fad95777e7bc09c5679556760b20790b08ab
abichan99 <132280033+abichan99@users.noreply.github.com>
2024-08-17 08:59:13 +0000

    13.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
+  has_many :microposts
+
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
 
